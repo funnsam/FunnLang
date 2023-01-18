989d66196010397c58fb894a0f531b0a05888c89
emm312 <emmtbm55@gmail.com>
2023-01-18 11:36:06 +1100

    fix bugs

--- src/compiler/ast_compiler.rs
+++ src/compiler/ast_compiler.rs
@@ -11,7 +11,7 @@ pub fn compiler(prog: Program) -> Module {
     let mut variables: HashMap<String, VariableId> = HashMap::new();
 
     compile(prog, &mut builder, &mut functions, &mut variables);
-    builder.build()
+    builder.build().unwrap()
 }
 
 fn compile(prog: Program, builder: &mut ModuleBuilder, functions: &mut HashMap<String, FunctionId>, variables: &HashMap<String, VariableId>) {
@@ -81,75 +81,75 @@ fn compile(prog: Program, builder: &mut ModuleBuilder, functions: &mut HashMap<S
 
 fn compile_expr(expr: Expr, builder: &mut ModuleBuilder, functions: &mut HashMap<String, FunctionId>, variables: &HashMap<String, VariableId>, typ: &IRType) -> Value {
     match expr {
-        Expr::Number(n) => builder.push_instruction(n.to_integer_operation()).unwrap(),
+        Expr::Number(n) => builder.push_instruction(n.to_integer_operation()).unwrap().unwrap(),
         Expr::BoolOp { left, oper, right } => {
             match oper {
                 BoolOp::Add => {
                     let left = compile_expr(*left, builder, functions, variables, typ);
                     let right = compile_expr(*right, builder, functions, variables, typ);
-                    builder.push_instruction(Operation::Add(left, right)).unwrap()
+                    builder.push_instruction(Operation::Add(left, right)).unwrap().unwrap()
                 }
                 BoolOp::Sub => {
                     let left = compile_expr(*left, builder, functions, variables, typ);
                     let right = compile_expr(*right, builder, functions, variables, typ);
-                    builder.push_instruction(Operation::Sub(left, right)).unwrap()
+                    builder.push_instruction(Operation::Sub(left, right)).unwrap().unwrap()
                 }
                 BoolOp::Mul => {
                     let left = compile_expr(*left, builder, functions, variables, typ);
                     let right = compile_expr(*right, builder, functions, variables, typ);
-                    builder.push_instruction(Operation::Mul(left, right)).unwrap()
+                    builder.push_instruction(Operation::Mul(left, right)).unwrap().unwrap()
                 }
                 BoolOp::Div => {
                     let left = compile_expr(*left, builder, functions, variables, typ);
                     let right = compile_expr(*right, builder, functions, variables, typ);
-                    builder.push_instruction(Operation::Div(left, right)).unwrap()
+                    builder.push_instruction(Operation::Div(left, right)).unwrap().unwrap()
                 }
                 BoolOp::Mod => {
                     let left = compile_expr(*left, builder, functions, variables, typ);
                     let right = compile_expr(*right, builder, functions, variables, typ);
-                    builder.push_instruction(Operation::Mod(left, right)).unwrap()
+                    builder.push_instruction(Operation::Mod(left, right)).unwrap().unwrap()
                 }
                 BoolOp::And => {
                     let left = compile_expr(*left, builder, functions, variables, typ);
                     let right = compile_expr(*right, builder, functions, variables, typ);
-                    builder.push_instruction(Operation::BitAnd(left, right)).unwrap()
+                    builder.push_instruction(Operation::BitAnd(left, right)).unwrap().unwrap()
                 }
                 BoolOp::Or => {
                     let left = compile_expr(*left, builder, functions, variables, typ);
                     let right = compile_expr(*right, builder, functions, variables, typ);
-                    builder.push_instruction(Operation::BitOr(left, right)).unwrap()
+                    builder.push_instruction(Operation::BitOr(left, right)).unwrap().unwrap()
                 }
                 BoolOp::XOr => {
                     let left = compile_expr(*left, builder, functions, variables, typ);
                     let right = compile_expr(*right, builder, functions, variables, typ);
-                    builder.push_instruction(Operation::BitXor(left, right)).unwrap()
+                    builder.push_instruction(Operation::BitXor(left, right)).unwrap().unwrap()
                 }
             }
         }
         Expr::Ident(i) => {
-            builder.push_instruction(Operation::GetVar(variables[&i])).unwrap()
+            builder.push_instruction(Operation::GetVar(variables[&i])).unwrap().unwrap()
         }
         Expr::CompOp { left, oper, right } => {
             let lhs = compile_expr(*left, builder, functions, variables, typ);
             let rhs = compile_expr(*right, builder, functions, variables, typ);
             match oper {
                 CompOp::EQ => {
-                    builder.push_instruction(Operation::Eq(lhs, rhs)).unwrap()
+                    builder.push_instruction(Operation::Eq(lhs, rhs)).unwrap().unwrap()
                 }
                 CompOp::NEQ => {
-                    builder.push_instruction(Operation::Ne(lhs, rhs)).unwrap()
+                    builder.push_instruction(Operation::Ne(lhs, rhs)).unwrap().unwrap()
                 }
                 CompOp::GT => {
-                    builder.push_instruction(Operation::Gt(lhs, rhs)).unwrap()
+                    builder.push_instruction(Operation::Gt(lhs, rhs)).unwrap().unwrap()
                 }
                 CompOp::GTE => {
-                    builder.push_instruction(Operation::Ge(lhs, rhs)).unwrap()
+                    builder.push_instruction(Operation::Ge(lhs, rhs)).unwrap().unwrap()
                 }
                 CompOp::LT => {
-                    builder.push_instruction(Operation::Lt(lhs, rhs)).unwrap()
+                    builder.push_instruction(Operation::Lt(lhs, rhs)).unwrap().unwrap()
                 }
                 CompOp::LTE => {
-                    builder.push_instruction(Operation::Le(lhs, rhs)).unwrap()
+                    builder.push_instruction(Operation::Le(lhs, rhs)).unwrap().unwrap()
                 }
             }
         },
